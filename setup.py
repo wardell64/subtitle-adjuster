3414af5f0990ba8877cb428dfcb9903dd876ad40
Michael Wardell <77124258+wardell64@users.noreply.github.com>
2023-09-26 20:22:13 -0700

    Create setup.py
    Still WIP

---
+++ setup.py
@@ -0,0 +1,24 @@
+"""
+make me a wheel
+"""
+from setuptools import setup
+
+setup(
+    name='subtitle-adjuster',
+    version='1.0.0',
+    description='Subtitle Adjuster',
+    url='https://github.com/wardell64/subtitle-adjuster',
+    author='Michael Wardell',
+    author_email='michaelwardell@gmail.com',
+    license='GPLv3+',
+    packages=['subtitle-adjuster'],
+    install_requires=[],
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: Microsoft :: Windows',
+        'Programming Language :: Python :: 3',
+    ],
+)
